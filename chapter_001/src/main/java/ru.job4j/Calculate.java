13489536c1bd90bf76fbbfefd5d759f6b08c3438
Andrey Lisovtsev <alisovtsev@gmail.com>
2016-12-12 18:17:31 +0700

    1.3 added java doc

--- chapter_001/src/main/java/ru.job4j/Calculate.java
+++ chapter_001/src/main/java/ru.job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+ * Class for calculate arifmetic operations + - * / ^.
+ * @author alisovtsev
+ * @since 12.12.2016
+ * @version 1
+*/
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello world");
